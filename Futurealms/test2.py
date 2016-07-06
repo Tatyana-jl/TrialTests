0d2fb0b35b04696b06697e051782f696abe7e391
Sonyachna <Sonyachna>
2016-07-06 09:00:48 +0300

    Test 2

---
+++ Futurealms/test2.py
@@ -0,0 +1,17 @@
+import numpy as np
+import sklearn
+from sklearn.metrics import confusion_matrix, precision_score
+from sklearn.datasets import load_iris
+from sklearn import metrics, ensemble
+from sklearn.cross_validation import train_test_split
+iris=load_iris()
+y = iris['target']
+x = iris['data']
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.33, random_state=42)
+model = sklearn.ensemble.GradientBoostingClassifier()
+model.fit(x_train, y_train)
+expected = y_test
+predicted = model.predict(x_test)
+print(metrics.classification_report(expected, predicted))
+print(metrics.confusion_matrix(expected, predicted))
+print(model.score(x_test, y_test))
